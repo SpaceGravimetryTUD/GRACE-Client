3f6c7db6b9e9c73c793bd888c891bf72f4daa617
joaoxg <j.guimaraes@tudelft.nl>
2025-08-28 18:23:39 +0200

    update test_sql.py

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -7,15 +7,16 @@ db_envname = "DATABASE_URL"
 tbl_envname = "TABLE_NAME"
 required_columns = ["id","datetime","latitude_A","longitude_A","postfit","up_combined"]
 
-@pytest.fixture(scope="session")
 def engine():
+    assert create_engine(os.getenv(db_envname)), "No database  found with the following URL: " + str(os.getenv(db_envname))
     return create_engine(os.getenv(db_envname))
 
-def test_table_exists(engine):
+def test_table_exists(engine=engine()):
     inspector = inspect(engine)
     tables = inspector.get_table_names()
-    assert os.getenv(tbl_envname) in tables
+    assert os.getenv(tbl_envname) in tables, "No table found in the database with the following name: " + str(os.getenv(tbl_envname))
 
-def test_required_columns(engine):
+def test_required_columns(engine=engine()):
     allowed_columns = _get_allowed_columns(engine)
-    for col in required_columns: assert col in allowed_columns
+    for col in required_columns:
+        assert col in allowed_columns, "The following requested column could not be found back in the database table: " + col
